24b5f8eebf3e7419ad7f99814b80b8f8ee6105d9
jacobef <jacobef2@gmail.com>
2022-10-29 12:38:17 -0600

    integrate 'login/register' page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from accounts import views
 app_name = "accounts"
 
 urlpatterns = [
+    path('login_register/', views.login_register, name='login_register'),
     path('login/', views.Login.as_view(), name='login'),
     path('create/', views.CreateAccount.as_view(), name='create'),
     path('create/success/', views.account_created, name='account_created'),
